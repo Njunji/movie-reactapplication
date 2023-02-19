d5a3eb22c76edb818230ebf504c2e4154367a757
Njunji Muchai <samwelnjunji@gmail.com>
2023-02-20 02:17:24 +0300

    added search Function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import {useEffect} from 'react';
 
+import './App.css';
+import SearchIcon from './search.svg';
+
 const API_URL = 'http://www.omdbapi.com?apikey=3baf1012';
 
 const App = () => {
@@ -17,7 +20,28 @@ const App = () => {
 
 
     return(
-        <h1>App</h1>
+        <div className="app">
+            <h1>Movie Land</h1>
+
+            <div className="search">
+                <input
+                    placeholder='Search for movies'
+                    value="Fifty Shades of Grey"
+                    onChange={() => {}}
+                />
+
+                <img
+                src={SearchIcon}
+                alt="search"
+                onClick = {() => {}}
+                />
+            </div>
+
+            <div className="container">
+            
+            </div>
+
+        </div> 
     );
 }
 
