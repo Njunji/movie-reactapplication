035a2725b54ebdf90f81662015a9beef1e4bce50
Njunji Muchai <samwelnjunji@gmail.com>
2023-02-20 00:50:53 +0300

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import {useEffect} from 'react';
+
+const API_URL = 'http://www.omdbapi.com?apikey=3baf1012';
 
 const App = () => {
+
+    const searchMovies = async (title) => {}
+
+    useEffect(() => {
+
+    },[]);
+
+
     return(
         <h1>App</h1>
     );
